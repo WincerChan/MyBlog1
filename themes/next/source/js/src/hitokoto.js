9c8f5d9cf5e67cc3b8701a32f5c7bf92e7bf99ed
WincerChan <WincerChan@jxnu.edu.cn>
2017-10-04 12:27:14 +0800

    Update hitokoto length in .js

--- themes/next/source/js/src/hitokoto.js
+++ themes/next/source/js/src/hitokoto.js
@@ -1,4 +1,4 @@
 // var Str = JSON.stringify(hitokoto);
 // var Arr = JSON.parse(Str);
-var rand = Math.floor(Math.random() * 1391);
+var rand = Math.floor(Math.random() * 1231);
 document.write(arr[rand].hitokoto + '   ——「' + arr[rand].from + '」');
