f2d4def61711f7a3b7bae99cb87568850a6c3c83
WincerChan <WincerChan@jxnu.edu.cn>
2017-11-18 22:12:02 +0800

    Update NexT version to 5.1.3

--- themes/next/source/js/src/hitokoto.js
+++
@@ -1,4 +0,0 @@
-// var Str = JSON.stringify(hitokoto);
-// var Arr = JSON.parse(Str);
-var rand = Math.floor(Math.random() * 1231);
-document.write(arr[rand].hitokoto + '——「' + arr[rand].from + '」');
