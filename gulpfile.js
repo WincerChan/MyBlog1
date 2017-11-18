77ecd2a70db6b6d2c02e7d84c54a817051eef8d0
WincerChan <WincerChan@jxnu.edu.cn>
2017-11-18 22:48:29 +0800

    Add cover minify

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,9 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),
     runSequence = require('run-sequence'),
     Hexo = require('hexo');
-    
+
 //清除public
-gulp.task('clean', function(){
+gulp.task('clean', function() {
     return del(['public/**/*']);
 });
 // 利用Hexo API 来生成博客内容， 效果和在命令行运行： hexo g 一样
@@ -23,13 +23,13 @@ gulp.task('generate', function(cb) {
     }).then(function() {
         return hexo.exit();
     }).then(function() {
-        return cb()
+        return cb();
     }).catch(function(err) {
         console.log(err);
         hexo.exit(err);
         return cb(err);
-    })
-})
+    });
+});
 //JS压缩
 gulp.task('uglify', function() {
     return gulp.src('././public/js/*.js')
@@ -66,25 +66,51 @@ gulp.task('minify-html', function() {
             minifyCSS: true,
             minifyURLs: true,
         }))
-        .pipe(gulp.dest('./public'))
+        .pipe(gulp.dest('./public'));
 });
 // 同上，压缩图片，这里采用了： 最大化压缩效果。
 gulp.task('minify-img-aggressive', function() {
     return gulp.src('./public/images/**/*.*')
         .pipe(imagemin(
-        [imagemin.gifsicle({'optimizationLevel': 3}), 
-        imagemin.jpegtran({'progressive': true}), 
-        imagemin.optipng({'optimizationLevel': 7}), 
-        imagemin.svgo()],
-        {'verbose': true}))
-        .pipe(gulp.dest('./public/images'))
+            [imagemin.gifsicle({
+                    'optimizationLevel': 3
+                }),
+                imagemin.jpegtran({
+                    'progressive': true
+                }),
+                imagemin.optipng({
+                    'optimizationLevel': 7
+                }),
+                imagemin.svgo()
+            ], {
+                'verbose': true
+            }))
+        .pipe(gulp.dest('./public/images'));
+})
+gulp.task('minify-img-cover', function() {
+    return gulp.src('./public/life/cover/*.*')
+        .pipe(imagemin(
+            [imagemin.gifsicle({
+                'optimizationLevel': 3
+            }),
+             imagemin.jpegtran({
+                 'progressive': true
+             }),
+             imagemin.optipng({
+                 'optimizationLevel': 7
+             }),
+             imagemin.svgo()
+            ], {
+                'verbose': true
+            }))
+        .pipe(gulp.dest('./public/life/cover'))
 })
 // 用run-sequence并发执行，同时处理html，css，js，img
 gulp.task('compress', function(cb) {
-    runSequence(['minify-html', 'cssmin', 'uglify', 'fancybox:js', 'fancybox:css', 'minify-img-aggressive'], cb);
+    runSequence(['minify-html', 'cssmin', 'uglify', 'fancybox:js', 'fancybox:css', 'minify-img-aggressive', 'minify-img-cover'], cb);
 });
 // 执行顺序： 清除public目录 -> 产生原始博客内容 -> 执行压缩混淆
 gulp.task('build', function(cb) {
-    runSequence('clean', 'generate', 'compress', cb)
+    runSequence('clean', 'generate', 'compress', cb);
 });
-gulp.task('default', ['build'])
+gulp.task('default', ['build']);
